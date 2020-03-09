e9141f8ed38296529522600fb5488ddf0239fa1b
comanezz <aymeric.lcharles@gmail.com>
2020-03-09 20:11:39 +0000

    Custom jQuesry added, allowing to check if all the field of the form is filled

---
+++ static/js/form-validation.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $("#submit-btn").click(function() {
+        var author_name = $("#author_name").val();
+        var servings = $("#servings").val();
+        var recipe_name = $("#recipe_name").val();
+        var description = $('#description').val();
+        var recipe_type = $('#recipe_type').val();
+        var Preparation_time = $('#Preparation_time').val();
+        var cooking = $('#cooking').val();
+        var ingredients = $('#ingredients').val();
+        var methods = $('#methods').val();
+
+        if (author_name == '' || servings == '' || recipe_name == ''|| description == ''|| 
+            recipe_type == ''|| Preparation_time == ''|| cooking == ''|| ingredients == ''|| methods == '') {
+            alert("Please fill all the fields !")
+            return false;
+        } else {
+            alert('You have successfully registered a recipe')
+        }
+    });
+});
