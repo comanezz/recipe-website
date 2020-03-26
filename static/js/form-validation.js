20b907568db59c339c587fad75f25622397a546e
comanezz <aymeric.lcharles@gmail.com>
2020-03-26 18:34:13 +0000

    Comment added to the function and variable. More clear

--- static/js/form-validation.js
+++ static/js/form-validation.js
@@ -7,9 +7,9 @@ $(document).ready(function() {
         var cooking = $('#cooking').val();
         var ingredients = $('#ingredients').val();
         var methods = $('#methods').val();
-        /* Required attr not working on the select tag with materialize - Found a workaround with tutor
-        This replace the select options elements because 
-        they are actually hidden from view, and replaced with something else. */
+        /* Required attr not working on the select tag with materialize.
+        The select option tag was hidden and replaced by a unlisted list. So we have to target it.
+        - Found this workaround with tutor */
         var select_options = $(".dropdown-content");
         var options_list = $(".dropdown-content li");
         var array_value = true;
@@ -17,6 +17,7 @@ $(document).ready(function() {
         // Check if the fields are not empty
         if (author_name !== '' && recipe_name !== '' && description !== '' && 
             Preparation_time !== '' && cooking !== ''&& ingredients !== '' && methods !== '' && options_list.hasClass("active", "selected")) {
+                // For function partially written by a tutor. Had to complete the if statement by myself.
                 for(var i; i<select_options.length; i++) {
                     // Check if it has not the class selected. This if function was taken from https://stackoverflow.com/questions/10539162/using-jquery-to-see-if-a-div-has-a-child-with-a-certain-class
                     if (!select_options[i].find('li.selected').length !== 0){
